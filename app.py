f87a86a5d00f426134a71e6748eb4a275f32f1bc
Vivek Kumar Gupta <vivekgupta3749@gmail.com>
2025-05-10 23:54:34 +0530

    add athlete_events csv file

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import  plotly.figure_factory as ff
 df = pd.read_csv("athlete_events.csv")
-region_df =  pd.read_csv(r"C:\Users\vivek gupta\Downloads\noc_regions.csv")
+region_df =  pd.read_csv("noc_regions.csv")
+
 
 df = preprocessor.preprocess(df , region_df)
 
